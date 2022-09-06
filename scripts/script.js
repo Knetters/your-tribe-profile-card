58d2797e8e728bb4c6b9530a77b305dbaacee9e1
ThomasScheepers <thomas@scheepers.com>
2022-09-06 13:12:07 +0200

    beaty

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,37 @@
 var demoButtons;
 
-function start () {
-  
+function start() {
+
   // Add event "click" to "demo buttons"
-  demoButtons = document.querySelectorAll ('.js-modify');
+  demoButtons = document.querySelectorAll('.js-modify');
   for (var i = 0; i < demoButtons.length; i++) {
-    demoButtons[i].addEventListener ('click', toggleEffect);
+    demoButtons[i].addEventListener('click', toggleEffect);
   }
-  
+
   // Add event "click" to "save buttons"
-  var saveButtons = document.querySelectorAll ('.js-save');
+  var saveButtons = document.querySelectorAll('.js-save');
   for (var i = 0; i < saveButtons.length; i++) {
-    saveButtons[i].addEventListener ('click', toggleActive);
+    saveButtons[i].addEventListener('click', toggleActive);
   }
-  
+
 }
 
 // Toggle "effect" classes
-function toggleEffect () {
-  var target = document.querySelector (this.dataset.target);
-      target.dataset.effect = this.dataset.effect;
-  
-  for (var i= 0; i < demoButtons.length; i++) {
-    demoButtons[i].classList.remove ('active');
+function toggleEffect() {
+  var target = document.querySelector(this.dataset.target);
+  target.dataset.effect = this.dataset.effect;
+
+  for (var i = 0; i < demoButtons.length; i++) {
+    demoButtons[i].classList.remove('active');
   }
-  
-  toggleActive.call (this);
+
+  toggleActive.call(this);
 }
 
 // Toggle "active" class
-function toggleActive () {
-  this.classList.toggle ('active');
+function toggleActive() {
+  this.classList.toggle('active');
 }
 
 // Invoke "start ()" function
-window.addEventListener ('load', start);
+window.addEventListener('load', start);
